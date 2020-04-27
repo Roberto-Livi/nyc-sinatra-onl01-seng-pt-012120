71b2f0a5f64fd832561fc98e6ab18d4920782606
Roberto Livi <robertomlivi@outlook.com>
2020-04-27 20:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+  
+  get '/figures/new' do
+    erb :"views/figures/new"
+  end
+  
 end
